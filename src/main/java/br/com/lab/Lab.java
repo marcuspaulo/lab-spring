fff5dd8623d166138a5f9bd187112162697c3278
Marcus Paulo <mpaulobr@gmail.com>
2020-05-03 00:54:43 -0300

    First commit

--- src/main/java/br/com/lab/Lab.java
+++ src/main/java/br/com/lab/Lab.java
@@ -4,14 +4,21 @@ import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lab")
 public class Lab {
 
-	@GetMapping
+	@GetMapping("/test")
 	public String lab() {
 		return "Lab: " + new Date(); 
 	}
+	
+	@GetMapping("/")
+	public String code(@RequestParam("code") String code) {
+		return "code: " + code; 
+	}
+	
 }
